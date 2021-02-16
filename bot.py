63ffe6f68f2b7b8856dcd6c642878255c451a4c1
Vladimir Stepanov <vladimirs314159@gmail.com>
2021-02-17 00:41:14 +0300

    нарастили немного мяса боту

--- bot.py
+++ bot.py
@@ -6,21 +6,21 @@ class Bot:
     def __init__(self, token, debug=False):
         self.token = token
 
-    def method(self, method_name, json_data):
+    def _telegram_method(self, method_name, json_data):
         url = f"https://api.telegram.org/bot{self.token}/{method_name}"
         r = requests.post(url, data=json_data)
-        if self.debug:
-            print(r.text)
+        # if self.debug:
+        #    print(r.text)
         return r
 
     def send_message(self, chat_id, text, keyboard_array=None, reply_to_message_id=None):
         data = {"chat_id": chat_id, "text": text}
         if keyboard_array is not None:
-            data["reply_markup"] = self.reply_keyboard(keyboard_array)
+            data["reply_markup"] = self._reply_keyboard(keyboard_array)
         if reply_to_message_id is not None:
             data["reply_to_message_id"] = reply_to_message_id
-        self.method("sendMessage", data)  # response: Response =
+        self._telegram_method("sendMessage", data)  # response: Response =
 
-    def reply_keyboard(self, keyboard_array):
+    def _reply_keyboard(self, keyboard_array):
         reply_markup = {"keyboard": keyboard_array, "resize_keyboard": True, "one_time_keyboard": True}
         return json.dumps(reply_markup)
