3828aa35712cf24dc2c956f2ee8a515613003f11
Vladimir Stepanov <vladimirs314159@gmail.com>
2021-02-17 00:51:44 +0300

    Рефакторинг + накидали методов

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 from flask import Flask, request, Response
-from telegramHandler import Bot
+from bot import Bot
 from datetime import datetime, timedelta
 
 # Made by catinapoke with Akenth0r
 
 TOKEN = '1623674677:AAFNHd1PgUbzKH3YW7nUoUGXKzEePGGq3tY'  # AAFNHd1PgUbzKH3YW7nUoUGXKzWePGGGq3tY
-API_URL = 'https://api.telegram.org/bot%s/sendMessage' % TOKEN
 DEBUG = False
 
 app = Flask(__name__)
-bot = Bot(TOKEN, DEBUG)
+bot = Bot(TOKEN)
 
 
-def handle_message(json_text: dict):
-    chat_id = request.json["message"]["chat"]["id"]
-    text = request.json["message"]["text"]
-    bot.send_message(chat_id, f"I got message \"{text}\"", [["Адин"], ["Два", "Три"]],
-                     reply_to_message_id=request.json["message"]["message_id"])
-    if DEBUG:
-        for var in request.json:
-            print(f'{var}; {request.json[var]}')
+def start_handler(message: dict):
+    bot.send_message(message['chat']['id'], 'Hello to you in back!')
+
+
+bot.add_handler('hello', start_handler)
+
+
+def handle_message(message: dict):
+    # chat_id = request.json["message"]["chat"]["id"]
+    # text = request.json["message"]["text"]
+    # bot.send_message(chat_id, f"I got message \"{text}\"", [["Адин"], ["Два", "Три"]],
+    #                  reply_to_message_id=request.json["message"]["message_id"])
+    # if DEBUG:
+    #     for var in request.json:
+    #         print(f'{var}; {request.json[var]}')
+    bot.handle_message(message['message'])
 
 
 def check_timestamp(timestamp):
@@ -33,6 +40,6 @@ def receive_update():
     if request.method == "POST" and not check_timestamp(request.json["message"]["date"]):
         handle_message(request.json)
 
-    return Response('{"ok": True}', status=200, mimetype='application/json')
+    return Response('{"ok": true}', status=200, mimetype='application/json')
 
 # https://flask-aiohttp.readthedocs.io/en/latest/
--- bot.py
+++ bot.py
@@ -1,26 +1,53 @@
 import requests
 import json
+import telegram_api as api
 
-
+# _handlers = ["methodName":  function, ...]
 class Bot:
-    def __init__(self, token, debug=False):
-        self.token = token
 
-    def _telegram_method(self, method_name, json_data):
-        url = f"https://api.telegram.org/bot{self.token}/{method_name}"
-        r = requests.post(url, data=json_data)
-        # if self.debug:
-        #    print(r.text)
-        return r
+    def __init__(self, token):
+        self.token = token
+        self._bot_url = f"https://api.telegram.org/bot{self.token}"
+        # Разные меню имя/пункты
+        self._menus = {}
+        # Обработчики команд/сообщений
+        self._handlers = {}
+
+    def handle_message(self, message):
+        if message['text'] in self._handlers.keys():
+            self._handlers[message['text']](message)
 
     def send_message(self, chat_id, text, keyboard_array=None, reply_to_message_id=None):
         data = {"chat_id": chat_id, "text": text}
         if keyboard_array is not None:
-            data["reply_markup"] = self._reply_keyboard(keyboard_array)
+            data["reply_markup"] = api.reply_keyboard(keyboard_array)
         if reply_to_message_id is not None:
             data["reply_to_message_id"] = reply_to_message_id
-        self._telegram_method("sendMessage", data)  # response: Response =
+        api.telegram_method(api.METHOD_SEND_MESSAGE, data, self._bot_url)
 
-    def _reply_keyboard(self, keyboard_array):
-        reply_markup = {"keyboard": keyboard_array, "resize_keyboard": True, "one_time_keyboard": True}
-        return json.dumps(reply_markup)
+    def add_menu(self, menu):
+        pass
+
+    def add_handler(self, command, handler):
+        print(f'registered command {command}')
+        self._handlers[command] = handler
+
+    def remove_handler(self, command):
+        pass
+
+
+class Settings:
+    def __init__(self, settings={}):
+        self._settings = settings
+
+   # Установить настройку
+    def set(self, key, value):
+        pass
+
+    # Получить настройку
+    def get(self, key, value):
+        pass
+
+    # Сохраняем что-то
+    def save(self):
+        pass
