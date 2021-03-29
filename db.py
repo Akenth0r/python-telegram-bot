b416c7dc4c708e5bf83be712bcc62483ca31dabf
Vladimir Stepanov <vladimirs314159@gmail.com>
2021-03-30 00:35:24 +0300

    Радостный коммит

--- db.py
+++ db.py
@@ -60,6 +60,7 @@ class ThemeWord(base):
     theme_id = Column(String, ForeignKey('theme.id'))
     original = Column(String, nullable=False)
     translation = Column(String, nullable=False)
+    example = Column(String, nullable=False)
 
 
 # Тема - массив слов и название
