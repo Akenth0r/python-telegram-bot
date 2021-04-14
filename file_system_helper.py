b16cb8d22ad3c04537096c83738ef4f4880f8703
Vladimir Stepanov <vladimirs314159@gmail.com>
2021-04-15 00:12:14 +0300

    Задание 4

--- file_system_helper.py
+++ file_system_helper.py
@@ -3,9 +3,15 @@ import json
 
 
 # Save json string to file
+from fpdf import FPDF
+
+
 def save_to_file(filepath: str, text: str):
-    with open(filepath, 'w', encoding='utf-8') as file:
-        file.write(text)
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=15)
+    pdf.cell(200, 10, text)
+    pdf.output(filepath)
 
 
 # Load json from file
